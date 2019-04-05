889287d3fab77f0108a90c084cec2a1ad412c378
Harald Jörg <haj@posteo.de>
2019-04-05 20:53:53 +0200

    Add the dev user to the configuration

--- config.sh
+++ config.sh
@@ -2,17 +2,24 @@
 export REPOSITORY=https://github.com/HaraldJoerg/Act.git
 export BRANCH=act-two
 
+# The user running the tests.
+# Depending on the setup, this might be yourself, 'vagrant' (as on a
+# Vagrant Debian box), 'ubuntu' (as on a Vagrant Ubuntu box).
+# Don't use $USER: Parts of the scripts run under sudo, where
+# $USER will be root.
+export ACTUSER=vagrant
+
 # A place for the software.  Git will add /Act to the path.
-export CHECKOUT=/home/vagrant/opt
+export CHECKOUT=/home/$ACTUSER/opt
 
 # A place for the data
-export ACTHOME=/home/vagrant/var/opt/Act
+export ACTHOME=/home/$ACTUSER/var/opt/Act
 
 # Database user (all databases) and password
 export DBUSER=act
 export DBPASS=act123
 
-# Sample conference
+# The name of the sample conference
 export CONFERENCE=demo
 
 # Per default, run cpanm without tests (saves several minutes).
