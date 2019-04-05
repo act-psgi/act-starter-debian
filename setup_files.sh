5aef25df4ae83b72204f2947444619fe5d7d3b73
Harald Jörg <haj@posteo.de>
2019-04-06 00:56:59 +0200

    Fix directory structure for wwwdocs

--- setup_files.sh
+++ setup_files.sh
@@ -37,7 +37,7 @@ mkdir --parents $ACTHOME/var
 
 # Create a conference
 mkdir --parents $ACTHOME/actdocs
-mkdir --parents $ACTHOME/wwwdocs
+mkdir --parents $ACTHOME/$CONFERENCE
 cp -r $CHECKOUT/Act/skel/actdocs $ACTHOME/actdocs/$CONFERENCE
 cp -r $CHECKOUT/Act/skel/wwwdocs $ACTHOME/$CONFERENCE/wwwdocs
 date
